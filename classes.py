ff483daf71208c5bf8dd919a5b94d52dd396ded5
Bigyin <sergei.aparin1955@yandex.ru>
2017-12-06 00:44:39 +0300

    Обновление классов героя и монстра и расчет атаки

--- classes.py
+++ classes.py
@@ -1,86 +1,63 @@
 import random
 # в качестве структур данных я брал словари
-atk_def = {'ph_atk': 'ph_def', 'mag_atk': 'mag_def', 'fr_atk': 'mag_def'}  # создано для определения типа защиты при данном типе атаки
 
 
 class Player:
-    def __init__(self, **kwargs):
-        # self.login = kwargs['login']
-        # self.money = kwargs['money']
-        # self.inventory = kwargs['inventory']
-        self.heroes = kwargs['heroes']
-        # self.x = kwargs['x0']
-        # self.y = kwargs['y0']
+    def __init__(self, heroes):
 
-    # def move_player(self, x, y):
-    #     self.x = x
-    #     self.y = y
-
-    # def set_money(self, delta_money):
-    #     self.money += delta_money
-
-    # def append_inventory(self, item):
-    #     if item in self.inventory.keys():
-    #         self.inventory[item] += 1
-    #     else:
-    #         self.inventory += {item: 1}
+        self.heroes = heroes
 
     def get_alive_heroes(self):
         return [i for i in self.heroes if i.alive is True]
 
 
 class Hero:
-    def __init__(self, **kwargs):
-        self.name = kwargs['name']
-        self.hp = kwargs['hp']
-        self.mp = kwargs['mp']
-        self.sp = kwargs['sp']
-        self.ph_atk = kwargs['ph_atk']
-        self.mag_atk = kwargs['mag_atk']
-        self.fr_atk = kwargs['fr_atk']
-        self.ph_def = kwargs['ph_def']
-        self.mag_def = kwargs['mag_def']
-        self.at_list = {'ph_atk': self.ph_atk, 'mag_atk': self.mag_atk, 'fr_atk': self.fr_atk} # ph_atk:{'size':5,'cost':2,'type':'ph'}
-        self.df_list = {'ph_def': self.ph_def, 'mag_def': self.mag_def}
+
+    def __init__(self, name, hp, mp, sp, at_list, df_list):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.sp = sp
+        self.at_list = at_list  # кортеж с объектами Attack
+        self.df_list = df_list  # кортеж с объектами Defense
         self.alive = True
 
-    def attack_list_filter(self):
-        for i in list(self.at_list.items()):  # удаляем из at_list несуществующие типы атак для данного героя
-            if i[1] is None:
-                self.at_list.pop(i[0])
-
     def __str__(self):
-        return '{}'.format(self.name)
+        return 'name:{},hp: {},mp: {},sp: {}'.format(self.name, self.hp, self.mp, self.sp)
 
     def get_hp(self):
         return self.hp
 
-    def hp_change(self, impact_type, impact_size):
-        self.hp -= (1 - self.df_list[atk_def[impact_type]]) * impact_size
+    def hp_change(self, attack):
+        defense = 0
+        for i in self.df_list:
+            if i.name == attack.defense:
+                defense = i.size
+            else:
+                defense = 0
+        self.hp -= (1 - defense * 0.01) * attack.size  # сюда можно заносить изменения в расчете урона
         if self.hp <= 0:
             self.alive = False
 
-    # непосредственно расчет атаки,пока добавлены только два типа атаки
-    def attack(self, monster, attack_type):
-        if self.at_list[attack_type]['type'] == 'sp' and self.at_list[attack_type]['cost'] <= self.sp:
-            self.sp -= self.at_list[attack_type]['cost']
-            monster.hp_change(attack_type, self.at_list[attack_type]['size'])
-        if self.at_list[attack_type]['type'] == 'mp' and self.at_list[attack_type]['cost'] <= self.mp:
-            self.mp -= self.at_list[attack_type]['cost']
-            monster.hp_change(attack_type, self.at_list[attack_type]['size'])
+
+    # непосредственно расчет атаки
+    def attack(self, monster, attack):
+        print(monster)
+        print('name: {} sp: {} mp: {}'.format(self.name, self.sp, self.mp))
+        if attack.atr_type == 'sp' and attack.cost <= self.sp:
+            self.sp -= attack.cost
+            monster.hp_change(attack)
+        if attack.atr_type == 'mp' and attack.cost <= self.mp:
+            self.mp -= attack.cost
+            monster.hp_change(attack)
+        print(monster)
+        print('name: {} sp: {} mp: {}'.format(self.name, self.sp, self.mp))
+        print(attack)
 
 
 class Enemy:
-    def __init__(self, **kwargs):
-        # self.inventory = kwargs['inventory']
-        self.monsters = kwargs['monsters']
-        # self.x = kwargs['x0']
-        # self.y = kwargs['y0']
-        pass
-
-    # def move_enemy(self, x, y):
-    #     # self.x = x
-    #     # self.y = y
+    def __init__(self, monsters):
+        self.monsters = monsters
 
     def get_alive_monsters(self):
         return [i for i in self.monsters if i.alive is True]
@@ -90,49 +67,75 @@ class Enemy:
 
 
 class Monster:
-    def __init__(self, **kwargs):
-        self.name = kwargs['name']
-        self.hp = kwargs['hp']
-        self.mp = kwargs['mp']
-        self.sp = kwargs['sp']
-        self.ph_atk = kwargs['ph_atk']
-        self.mag_atk = kwargs['mag_atk']
-        self.ph_def = kwargs['ph_def']
-        self.mag_def = kwargs['mag_def']
-        self.at_list = {'ph_atk': self.ph_atk, 'mag_atk': self.mag_atk}
-        self.df_list = {'ph_def': self.ph_def, 'mag_def': self.mag_def}
+
+    def __init__(self, name, hp, mp, sp, at_list, df_list):
+        self.name = name
+        self.hp = hp
+        self.mp = mp
+        self.sp = sp
+        self.at_list = at_list # кортеж с объектами Attack
+        self.df_list = df_list # кортеж с объектами Defense
         self.alive = True
 
     def __str__(self):
-        return '{}'.format(self.name)
+        return 'name: {}, hp: {},mp: {},sp: {}'.format(self.name, self.hp, self.mp, self.sp)
 
     def get_hp(self):
         return self.hp
 
     def choose_random_attack(self):
-        i = random.choice(self.at_list.values())
-        while i is None:
-            i = random.choice(self.at_list.values())
+        i = random.choice(self.at_list)
         return i
 
-    def hp_change(self, impact_type, impact_size):
-        self.hp -= (1 - self.df_list[atk_def[impact_type]] * 0.01) * impact_size
+    def hp_change(self, attack):
+        defense = 0
+        for i in self.df_list:
+            if i.name == attack.defense:
+                defense = i.size
+            else:
+                defense = 0
+        self.hp -= (1 - defense * 0.01) * attack.size  # сюда можно заносить изменения в расчете урона
         if self.hp <= 0:
             self.alive = False
 
     def attack(self, heroes):
-        hero = sorted(heroes, key=lambda hro: hro.get_hp())[0]
-        attack_type = self.choose_random_attack()
-        if self.at_list[attack_type]['type'] == 'sp' and self.at_list[attack_type]['cost'] >= self.sp:
-            self.sp -= self.at_list[attack_type]['cost']
-            hero.hp_change(attack_type, self.at_list[attack_type]['size'])
-        if self.at_list[attack_type]['type'] == 'mp' and self.at_list[attack_type]['cost'] >= self.mp:
-            self.mp -= self.at_list[attack_type]['cost']
-            hero.hp_change(attack_type, self.at_list[attack_type]['size'])
+        low_hp = sorted(heroes, key=lambda hro: hro.get_hp())[0:2] # рандомный выбор из двух героев с наименьшим хп
+        hero = random.choice(low_hp)
+        print(hero)
+        print('name: {} sp: {} mp: {}'.format(self.name, self.sp, self.mp))
+        attack = self.choose_random_attack()
+        if attack.atr_type == 'sp' and attack.cost <= self.sp:
+            self.sp -= attack.cost
+            hero.hp_change(attack)
+        if attack.atr_type == 'mp' and attack.cost <= self.mp:
+            self.mp -= attack.cost
+            hero.hp_change(attack)
+
+        print(hero)
+        print('name: {} sp: {} mp: {}'.format(self.name, self.sp, self.mp))
+        print(attack)
 
 
+class Attack:
+    def __init__(self, name, size, cost, atr_type, defense, features=None):
+        self.name = name
+        self.size = size
+        self.cost = cost
+        self.atr_type = atr_type
+        self.defense = defense # имя соотв защиты
+        self.features = features
+
+    def __str__(self):
+        return '{}'.format(self.name)
 
 
+class Defence:
+    def __init__(self, name, size):
+        self.name = name
+        self.size = size
+
+    def __str__(self):
+        return '{}'.format(self.name)
 
 
 
